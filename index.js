96c9789093b616aec82fc0f6f37cb2773fa6792a
Samuel Sharpe <samit4me@gmail.com>
2017-04-27 21:46:44 +1000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * Add two numbers.
+ * @param {number} num1 The first number.
+ * @param {number} num2 The second number.
+ * @returns {number} The sum of the two numbers.
+ */
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+const sum = add(1, 1);
